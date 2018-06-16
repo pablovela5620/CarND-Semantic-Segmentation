a8f97934b0bc82b82e309e04f31ae62767ed9cfa
pablo <pablovela5620@gmail.com>
2018-06-15 20:24:24 -0400

    Modified generator and file names to make code more understandable

--- helper.py
+++ helper.py
@@ -58,7 +58,7 @@ def maybe_download_pretrained_vgg(data_dir):
         os.remove(os.path.join(vgg_path, vgg_filename))
 
 
-def gen_batch_function(data_folder, image_shape):
+def gen_batch_function(train_dir, gt_dir, image_shape):
     """
     Generate function to create batches of training data
     :param data_folder: Path to folder that contains all the datasets
@@ -71,10 +71,8 @@ def gen_batch_function(data_folder, image_shape):
         :param batch_size: Batch Size
         :return: Batches of training data
         """
-        image_paths = glob(os.path.join(data_folder, 'image_2', '*.png'))
-        label_paths = {
-            re.sub(r'_(lane|road)_', '_', os.path.basename(path)): path
-            for path in glob(os.path.join(data_folder, 'gt_image_2', '*_road_*.png'))}
+        image_paths = glob(os.path.join(train_dir, '*.png'))
+        label_paths = glob(os.path.join(gt_dir, '*.png'))
         background_color = np.array([255, 0, 0])
 
         random.shuffle(image_paths)
@@ -82,15 +80,15 @@ def gen_batch_function(data_folder, image_shape):
             images = []
             gt_images = []
             for image_file in image_paths[batch_i:batch_i+batch_size]:
-                gt_image_file = label_paths[os.path.basename(image_file)]
+                gt_image_file = gt_dir+os.path.basename(image_file)
 
                 image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
                 gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
 
                 gt_bg = np.all(gt_image == background_color, axis=2)
                 gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
-                gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
-
+                gt_image = np.dstack((gt_bg, np.invert(gt_bg)))
+                
                 images.append(image)
                 gt_images.append(gt_image)
 
